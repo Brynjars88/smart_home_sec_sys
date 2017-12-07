3d7cf50aa4665e1321e96104804c8709f6dd1f18
BrynjarSig <brs24@hi.is>
2017-12-07 12:56:46 +0000

    Initalized project

---
+++ security_system.ino
@@ -0,0 +1,19 @@
+// Smart home security system with following sensors:
+// - Temperature
+// - Humidity
+// - Motion
+// - CO (for smoke detection)
+// 
+
+// Config
+#include "config.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
