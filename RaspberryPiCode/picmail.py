54a003fdd333b03c0b0a3b9bd7959c7baa4a63cf
Brynjars88 <brynjar.sig88@gmail.com>
2017-12-14 15:38:11 +0000

    Added Raspberry Pi code for taking picture and sending via email

---
+++ RaspberryPiCode/picmail.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import time
+from datetime import datetime
+import picamera
+import smtplib
+from smtplib import SMTP, SMTPException
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+with picamera.PiCamera() as camera:
+	camera.resolution = (1024, 768)
+	camera.start_preview()
+	# Camera warm-up time
+	time.sleep(2)
+	camera.capture('photo.jpg')
+
+f_time = datetime.now().strftime('%a %d %b @ %H:%M')
+
+toaddr = 'iotsecsys@gmail.com' 
+me = 'iotsecsys@gmail.com'
+subject = 'Motion detected: ' + f_time
+
+msg = MIMEMultipart()
+msg['Subject'] = subject
+msg['From'] = me
+msg['To'] = toaddr
+msg.preamble = "Photo @ " + f_time
+
+fp = open('photo.jpg', 'rb')
+img = MIMEImage(fp.read())
+fp.close()
+msg.attach(img)
+
+try:
+	s = smtplib.SMTP('smtp.gmail.com',587)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+	s.login(user = 'iotsecsys', password = 'qathkaubnqvfjksw')
+	s.send_message(msg)
+	s.sendmail(me, toaddr, msg.as_string())
+	s.quit()
+except SMTPException as e:
+	print("Error: unable to send email : {err}".format(err=e))
